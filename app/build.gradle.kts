4311762e65fc4b838f3d06f54330b6468837832e
Kirill Nayduik <kirill.nayduikkn1@gmail.com>
2025-03-03 18:32:47 +0300

    Initial implementation of WorkManagerSample

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlinx.serializer)
 }
 
 android {
@@ -44,8 +43,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.workmanager)
-    implementation(libs.kotlinx.serializer)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
